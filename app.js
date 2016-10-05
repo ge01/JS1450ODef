dc2eb7eeb580bf0d88cf3ce9290ce207a60bb64a
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-10-05 01:21:14 -0500

    Using an Object Constructor

--- app.js
+++ app.js
@@ -9,3 +9,30 @@ var person = {
 };
 
 document.getElementById("demo").innerHTML = person.firstName + " " + person.lastName;
+
+/*******************************************************************************
+*                    Using the JavaScript Keyword new                          *
+********************************************************************************/
+var person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+document.getElementById("demo1").innerHTML = person.firstName + " is " + person.age + " years old.";
+
+/*******************************************************************************
+*                        Using an Object Constructor                           *
+********************************************************************************/
+function person1(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+
+var myFather = new person1("John", "Doe", 50, "blue");
+var myMother = new person1("Sally", "Rally", 48, "green");
+
+document.getElementById("demo2").innerHTML =
+"My father is " + myFather.age + ". My mother is " + myMother.age;
